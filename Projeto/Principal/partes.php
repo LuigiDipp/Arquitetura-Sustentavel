54fdc48a8e905a26e88bcff5d540b8592d9633ed
JHRK <jhrk05@gmail.com>
2024-04-26 08:51:39 -0300

    dsa

--- Projeto/Principal/partes.php
+++ Projeto/Principal/partes.php
@@ -5,14 +5,20 @@ include 'header.php';
 <div class="col-12 row prin">  
     <div class="col-10 ola popup "><br>
         <h1>Biodigestor</h1>
-        <img src="./ASSETS/GIF BIODIGESTOR.gif" class="col-4 gifpartes" onclick="myFunction()"> 
-        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. A, dignissimos consequuntur deserunt quo quos ut delectus magni minus, ea voluptas ipsa ullam. Animi minima ea cupiditate est ab quos magnam!</p>
-    </div>
-    <div class="col-10  mt-3 ola popup " onclick="myFunction()"><br>
+        <img src="./ASSETS/GIF BIODIGESTOR.gif" class="col-4 gifpartes"> 
+        <p> Biodigestores são tanques que transformam matéria orgânica em biogás. <br>
+
+        Toda matéria orgânica que descartada pelos moradores será colocada dentro dos tanques.<br>  
+
+        Durante o processo de decomposição será extraído o gás metano para usos comuns.</p> </div>
+    <div class="col-10  mt-3 ola popup "><br>
         <h1>Composteira</h1>
         <img src="./ASSETS/GIF compsotetera.gif" class="col-4 gifpartes">
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium, quibusdam consequuntur! Amet tempora repudiandae a. Maxime omnis voluptatibus maiores quibusdam doloremque enim tempora deleniti? Vero illo excepturi magnam odio nemo.</p>
-    </div>  
+        <p> As composteiras serão preenchidas com os resíduos orgânicos produzido pelos moradores.<br>
+
+        São quatro compartimentos: Digestor 1, Digestor 2, Coletor e Tanque.<br>
+
+        Para produzir adubo, devem ser inseridas minhocas.</p></div>  
  </div>
 </div>
 </main>
