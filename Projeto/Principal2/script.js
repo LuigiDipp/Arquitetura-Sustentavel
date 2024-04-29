29b20dea8ac2c437af25d83fa5c69a35934d5ec6
João Pedro <joaopedropiguinho@gmail.com>
2024-04-29 08:51:24 -0300

    Atualização

---
+++ Projeto/Principal2/script.js
@@ -0,0 +1,6 @@
+document.getElementById('botaoo').addEventListener('click', function() {
+    this.classList.add('smoothHover');
+    setTimeout(() => {
+        this.classList.remove('smoothHover');
+    }, 300);
+});
