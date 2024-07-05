d9f4eae01cbdb1e807af15526489f712b8343d55
JHRK <jhrk05@gmail.com>
2024-07-05 18:46:23 -0300

    vhvnfng

--- Projeto/Principal/5.php
+++ Projeto/Principal/5.php
@@ -8,7 +8,7 @@
 
 <main class="container-fluid" id="main">
  <div class="col-12 row prin";>    
-
+    <h2>Repetir: Se possível, resíduos poderão retornar ao processo de uso por meio de reciclagem por mais vezes.</h2>
  </div>
 </main>
 </body>
